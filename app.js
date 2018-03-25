5ee97b4fecfa8b9a6b372a4026c22eaa78ffc5ea
sohit39 <gatiganti44914@sas.edu.sg>
2018-03-25 23:09:07 +0800

    ffff

--- app.js
+++ app.js
@@ -297,7 +297,7 @@ function getHoliday(sender, responseText, q1) {
 			let end = new Date(`${day["items"][0]["end"]["date"]}`);
 			var timeDiff = Math.abs(end.getTime() - start.getTime());
 			var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); //	converst from ms to days
-			var daysTillBreak = Math.floor((Math.abs((new Date()).getTime() - start.getTime())/(1000 * 3600 * 24)));
+			var daysTillBreak = Math.floor((new Date()).getTime() - start.getTime()/(1000 * 3600 * 24));
 			if (diffDays <= 1) {
 				let responses = `It's${responseText} ${day["items"][0]["summary"]} on ${day["items"][0]["start"]["date"]}`;
 				sendTextMessage(sender, responses);
@@ -383,6 +383,7 @@ function getSchoologyCourses(sender, responseText, schoologyUserID, tests) {
 			console.log("error");
 		}
 	});
+	sendTextMessage(sender, "Go get some work done!");
 }
 
 function getSchoologyCourseAssignments(sender, courseTitle, schoologyCourseID) {
@@ -604,7 +605,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 			}
 	
 		});
-		sendTextMessage(sender, "Go get some work done!");
+		
 		//end of fetching user data
 			
 		break;
