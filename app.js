f8ad8842fd8ccbea8f9650465d0f9ffa0a3ce907
sohit39 <gatiganti44914@sas.edu.sg>
2018-08-18 23:57:59 +0800

    course fix

--- app.js
+++ app.js
@@ -514,7 +514,7 @@ function getSchoologyCourses(sender, responseText, schoologyUserID, tests, speci
 				console.log("COURSE ID: " + courses["section"][j]["id"])
 				//sendTextMessage(sender, "You have course " + courses["section"][j]["course_title"] +  " with ID " + courses["section"][j]["id"]);
 				//getSchoologyCourseAssignments(sender, courses["section"][j]["course_title"], courses["section"][j]["id"]);
-				if(specificCourse != null && (courses["section"][j]["course_title"]).toLowerCase().indexOf(specificCourse.toLowerCase()) >= 0 || (specificCourse.toLowerCase().indexOf(courses["section"][j]["course_title"]).toLowerCase()) >= 0) {
+				if(specificCourse != null && (courses["section"][j]["course_title"]).toLowerCase().indexOf(specificCourse.toLowerCase()) >= 0 || (specificCourse.toLowerCase().indexOf(courses["section"][j]["course_title"].toLowerCase())) >= 0) {
 					getSchoologyCourseEvents(sender, courses["section"][j]["course_title"], courses["section"][j]["id"], specificDate);
 					sent = true;
 				}
